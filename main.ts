3d5fd8c3fa4110c6be13d28c47cec203e8f298f0
emilioprofe <132898547+emilioprofe@users.noreply.github.com>
2023-05-15 12:58:00 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,53 +1,31 @@
-function MOVIMIENTOIZQUIERDA () {
-    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
-    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 100)
-    basic.pause(500)
-}
-function INTERMITENTEIZDO () {
-    maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
-    maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
-    basic.pause(200)
-    maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
-    maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
-    basic.pause(200)
-}
-function INTERMITENTEDERECHO () {
-    maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
-    maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
-    basic.pause(200)
-    maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
-    maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
-    basic.pause(200)
-}
-function DOBLEINTERMITENTE () {
-    maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
-    maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
-}
-function MOVIMIENTODERECHA () {
-    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 100)
-    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
-    basic.pause(500)
-}
 let IZQUIERDA = false
 let strip = neopixel.create(DigitalPin.P15, 4, NeoPixelMode.RGB)
 basic.showIcon(IconNames.Happy)
 basic.pause(2000)
 basic.forever(function () {
-    basic.showString("EMLILIO")
-    music.playMelody("G B A G C5 B A B ", 120)
+    basic.showString("EMILIO")
+    strip.showRainbow(1, 360)
+    maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
+    maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
     if (maqueen.Ultrasonic(PingUnit.Centimeters) < 20 && maqueen.Ultrasonic(PingUnit.Centimeters) != 0) {
         IZQUIERDA = Math.randomBoolean()
-        strip.showRainbow(1, 360)
         if (IZQUIERDA == true) {
-            INTERMITENTEIZDO()
-            MOVIMIENTOIZQUIERDA()
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
+            maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
+            maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
+            basic.pause(100)
         }
         if (IZQUIERDA == false) {
-            INTERMITENTEDERECHO()
-            MOVIMIENTODERECHA()
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
+            maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
+            maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
+            basic.pause(100)
         }
     } else {
-        DOBLEINTERMITENTE()
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
+        basic.pause(100)
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 0)
     }
 })

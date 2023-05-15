d5d965b1cca3e5bd4600f96dff9023ec04318ca0
emilioprofe <132898547+emilioprofe@users.noreply.github.com>
2023-05-15 12:09:47 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,53 @@
+function MOVIMIENTOIZQUIERDA () {
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 100)
+    basic.pause(500)
+}
+function INTERMITENTEIZDO () {
+    maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
+    maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
+    basic.pause(200)
+    maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
+    maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
+    basic.pause(200)
+}
+function INTERMITENTEDERECHO () {
+    maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
+    maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
+    basic.pause(200)
+    maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
+    maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
+    basic.pause(200)
+}
+function DOBLEINTERMITENTE () {
+    maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
+    maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
+}
+function MOVIMIENTODERECHA () {
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 100)
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
+    basic.pause(500)
+}
 let IZQUIERDA = false
+let strip = neopixel.create(DigitalPin.P15, 4, NeoPixelMode.RGB)
+basic.showIcon(IconNames.Happy)
+basic.pause(2000)
 basic.forever(function () {
+    basic.showString("EMLILIO")
+    music.playMelody("G B A G C5 B A B ", 120)
     if (maqueen.Ultrasonic(PingUnit.Centimeters) < 20 && maqueen.Ultrasonic(PingUnit.Centimeters) != 0) {
         IZQUIERDA = Math.randomBoolean()
+        strip.showRainbow(1, 360)
         if (IZQUIERDA == true) {
-            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 0)
-            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 100)
-            basic.pause(500)
+            INTERMITENTEIZDO()
+            MOVIMIENTOIZQUIERDA()
         }
         if (IZQUIERDA == false) {
-            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 100)
-            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 0)
-            basic.pause(500)
+            INTERMITENTEDERECHO()
+            MOVIMIENTODERECHA()
         }
     } else {
+        DOBLEINTERMITENTE()
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
     }
 })
